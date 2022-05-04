c705f96be66ad42e680c84d975c5bf97a7d5f253
gcat <66452181+catagaboo@users.noreply.github.com>
2022-05-04 09:50:57 -0700

    Delete truck.py

--- truck.py
+++
@@ -1,8 +0,0 @@
-# Truck class creates truck objects
-class Truck:
-    def __init__(self):
-        self.speed = 0.3  # miles per minute
-        self.miles = 0
-        self.packages = []
-        self.time = 0
-        self.node_index = 1
